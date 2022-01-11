e61cda86bccaf1686f5b239e20ff0942654afd6f
Daniel Felipe Zamora Ortiz <D4n7elfelipe@gmail.com>
2022-01-11 13:41:29 -0500

    add role to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_09_223157) do
+ActiveRecord::Schema.define(version: 2022_01_11_183806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2022_01_09_223157) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role", default: 3
   end
 
 end
