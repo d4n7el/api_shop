0fc814aff7e19f1e6057127540f20de43c18cc42
Daniel Felipe Zamora Ortiz <D4n7elfelipe@gmail.com>
2022-01-18 15:58:30 -0500

    add migration product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_12_203244) do
+ActiveRecord::Schema.define(version: 2022_01_18_205454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 2022_01_12_203244) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "store_id", null: false
     t.index ["store_id"], name: "index_categories_on_store_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "stores", force: :cascade do |t|
@@ -47,5 +57,6 @@ ActiveRecord::Schema.define(version: 2022_01_12_203244) do
   end
 
   add_foreign_key "categories", "stores"
+  add_foreign_key "products", "categories"
   add_foreign_key "users", "stores"
 end
